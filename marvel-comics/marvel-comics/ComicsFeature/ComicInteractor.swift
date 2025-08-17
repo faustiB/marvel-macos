7415a3dcdf66f3ec8df9f9daf83ef848760be8c0
Faozi Bouybaouene Gadrouz <fbg171@gmail.com>
2025-08-17 18:06:15 +0200

    implemented interactor

--- marvel-comics/marvel-comics/ComicsFeature/ComicInteractor.swift
+++ marvel-comics/marvel-comics/ComicsFeature/ComicInteractor.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+protocol ComicsInteractorProtocol {
+  func fetchComics(limit: Int, offset: Int) async throws -> MarvelResponse<Comic>
+}
+
+// MARK: - Comics Interactor
+class ComicsInteractor: ComicsInteractorProtocol {
+  private let apiManager: APIManagerProtocol
+  
+  init(apiManager: APIManagerProtocol = APIManager()) {
+    self.apiManager = apiManager
+  }
+  
+  func fetchComics(limit: Int = 20, offset: Int = 0) async throws -> MarvelResponse<Comic> {
+    let parameters = [
+      "limit": String(limit),
+      "offset": String(offset),
+      "orderBy": "-modified"
+    ]
+    
+    return try await apiManager.request(
+      endpoint: "/comics",
+      parameters: nil,
+      responseType: MarvelResponse<Comic>.self
+    )
+  }
+}
