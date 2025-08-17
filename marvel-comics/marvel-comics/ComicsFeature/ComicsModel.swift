815e6a788a92f13d469d751e7b86513b5368b1d0
Faozi Bouybaouene Gadrouz <fbg171@gmail.com>
2025-08-17 18:07:02 +0200

    Implemented comics model

--- marvel-comics/marvel-comics/ComicsFeature/ComicsModel.swift
+++ marvel-comics/marvel-comics/ComicsFeature/ComicsModel.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+import Foundation
+
+struct Comic: Codable, Identifiable {
+  let id: Int
+  let digitalId: Int
+  let title: String
+  let issueNumber: Double
+  let variantDescription: String
+  let description: String?
+  let isbn: String
+  let pageCount: Int
+  let resourceURI: String
+  let urls: [ComicURL]
+  let dates: [ComicDate]
+  let prices: [ComicPrice]
+  let creators: CreatorList
+}
+
+struct ComicURL: Codable {
+  let type: String
+  let url: String
+}
+
+struct ComicDate: Codable {
+  let type: String
+  let date: String
+}
+
+struct ComicPrice: Codable {
+  let type: String
+  let price: Double
+}
+
+struct CreatorList: Codable {
+  let available: Int
+  let collectionURI: String
+  let items: [CreatorSummary]
+  let returned: Int
+}
+
+struct CreatorSummary: Codable {
+  let resourceURI: String
+  let name: String
+  let role: String
+}
