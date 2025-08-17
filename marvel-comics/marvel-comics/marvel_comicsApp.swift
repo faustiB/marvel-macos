0ed0bea5e83083cb1b87efc1f3381948a3000aae
Faozi Bouybaouene Gadrouz <fbg171@gmail.com>
2025-08-17 16:50:49 +0200

    initial commit of project

---
+++ marvel-comics/marvel-comics/marvel_comicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  marvel_comicsApp.swift
+//  marvel-comics
+//
+//  Created by Faozi Bouybaouene on 17/8/25.
+//
+
+import SwiftUI
+
+@main
+struct marvel_comicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
